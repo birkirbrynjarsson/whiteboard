df7218c8fd00f0cd30ac4943918335a0992302ef
Birkir Brynjarsson <birkir@birkir.is>
2026-04-20 13:12:14 +0000

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,53 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
+
+var paths = {
+    scss: 'app/scss/**/*.scss',
+    css: 'app/css/**/*.css',
+    html: 'app/*.html',
+    js: 'app/js/**/*.js'
+};
 
 /*
     GULP TASKS
 */
 
 // BrowserSync settings
-gulp.task('browserSync', function() {
+function browserSyncTask(done) {
     browserSync.init(null, {
         server: {
             baseDir: 'app'
         }
     });
-});
+    done();
+}
 
 // A real task takes in two additional gulp methods, gulp.src and gulp.dest
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss') // What files to use
+function sassTask() {
+    return gulp.src(paths.scss) // What files to use
         .pipe(sass()) // Sends file through plugin // using gulp-sass
         .pipe(gulp.dest('app/css')); // Where to output the files
-});
+}
 
 /*
     GULP WATCH
     Watches files, executes tasks on changes
 */
 
-gulp.task('watch', function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/css/**/*.css', browserSync.reload);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
+function watchFiles() {
+    gulp.watch(paths.scss, sassTask);
+    gulp.watch([paths.css, paths.html, paths.js]).on('change', browserSync.reload);
+}
 
 /*
     SERVER TASK
     Default task to serve up a server and start watching files for changes
 */
 
-gulp.task('serve', ['browserSync', 'sass', 'watch'], function() {
+var serve = gulp.series(sassTask, browserSyncTask, watchFiles);
 
-});
+exports.sass = sassTask;
+exports.watch = watchFiles;
+exports.serve = serve;
+exports.default = serve;
